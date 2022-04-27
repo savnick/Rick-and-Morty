3977dfdd7729969b614bf605eeca9cb44d5cccf0
Nick Savenkov <savnick@icloud.com>
2022-04-27 12:13:06 +0300

    Initial Commit

---
+++ Morty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Morty
+//
+//  Created by Nick on 27.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Morty/MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortyApp.swift
+//  Morty
+//
+//  Created by Nick on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
